ac99e91e5f233ac73ff1900c5442a09a47e46d20
SonicGD <sonicgd@gmail.com>
2018-10-30 10:47:53 +0500

    #19 Use autocomplete for properties dropdowns, map data

--- src/app/@services/properties.service.ts
+++ src/app/@services/properties.service.ts
@@ -6,33 +6,36 @@ import {ListResult} from '../@common/list/ListResult';
 import {Injectable} from '@angular/core';
 import {PropertiesOption} from '../@models/base/Properties';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class PropertiesService extends BaseService<PropertiesOption> {
-  constructor(httpClient: RestClient) {
-    super(httpClient);
-  }
+    constructor(httpClient: RestClient) {
+        super(httpClient);
+    }
 
-  public getOptions(groupKey: string, propertyKey: string): Observable<PropertiesOption[]> {
-    return this.httpClient.get(this.getResource(), {
-      setKey: groupKey,
-      propertyKey: propertyKey,
-    });
-  }
+    public getOptions(groupKey: string, propertyKey: string): Observable<PropertiesOption[]> {
+        return this.httpClient.get(this.getResource(), {
+            setKey: groupKey,
+            propertyKey: propertyKey,
+        }).pipe(map((result: ListResult<PropertiesOption>) => {
+            return result.Data;
+        }));
+    }
 
-  protected getResource(): string {
-    return 'properties';
-  }
+    protected getResource(): string {
+        return 'properties';
+    }
 
-  protected getListType(): ClassType<ListResult<PropertiesOption>> {
-    return null;
-  }
+    protected getListType(): ClassType<ListResult<PropertiesOption>> {
+        return null;
+    }
 
-  protected getSaveType(): ClassType<SaveModelResponse<PropertiesOption>> {
-    return null;
-  }
+    protected getSaveType(): ClassType<SaveModelResponse<PropertiesOption>> {
+        return null;
+    }
 
-  protected getType(): ClassType<PropertiesOption> {
-    return PropertiesOption;
-  }
+    protected getType(): ClassType<PropertiesOption> {
+        return PropertiesOption;
+    }
 }
