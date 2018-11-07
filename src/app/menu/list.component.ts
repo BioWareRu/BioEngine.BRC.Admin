c5776fca1a5bda0f7b894259581a110dfc70a857
SonicGD <sonicgd@gmail.com>
2018-11-07 22:44:10 +0500

    SiteIds -> SiteId

--- src/app/menu/list.component.ts
+++ src/app/menu/list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ListComponent} from '../@common/list/ListComponent';
-import {ListTableColumn, SitesTableColumn} from '../@common/list/ListTableColumn';
+import {ListTableColumn, SitesTableColumn, SiteTableColumn} from '../@common/list/ListTableColumn';
 import {ListTableColumnType} from '../@common/list/ListEnums';
 import {ListTableColumnAction} from '../@common/list/ListTableColumnAction';
 import {PageContext} from '../@common/PageComponent';
@@ -42,7 +42,7 @@ export class MenuListComponent extends ListComponent<Menu> implements OnInit {
         })
       /*.setDisabled(!this.can(UserRights.AddNews))*/,
       new ListTableColumn<Menu>('DateAdded', 'Дата', ListTableColumnType.TimeAgo).setSortable(),
-      new SitesTableColumn<Menu>('SiteIds', 'Сайты', this.sites),
+      new SiteTableColumn<Menu>('SiteId', 'Сайт', this.sites),
       new ListTableColumn<Menu>('Actions', '')
         .AddAction(
             new ListTableColumnAction<Menu>('Удалить', 'delete').setClick(menu => this.deleteItem(menu)),
