5a4f2de4cd1d3b78940b365a48fa741954610843
George <sonicgd@gmail.com>
2019-03-23 20:40:28 +0500

    #160 add link to main page from post editor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,34 +1,38 @@
-import {Component} from '@angular/core';
-import {map} from 'rxjs/operators';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { PageComponent, PageContext } from 'app/@common/PageComponent';
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
+    providers: [PageContext]
 })
-export class DashboardComponent {
+export class DashboardComponent extends PageComponent {
     /** Based on the screen size, switch from standard to one column per row */
     cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-        map(({matches}) => {
+        map(({ matches }) => {
             if (matches) {
                 return [
-                    {title: 'Card 1', cols: 1, rows: 1},
-                    {title: 'Card 2', cols: 1, rows: 1},
-                    {title: 'Card 3', cols: 1, rows: 1},
-                    {title: 'Card 4', cols: 1, rows: 1}
+                    { title: 'Card 1', cols: 1, rows: 1 },
+                    { title: 'Card 2', cols: 1, rows: 1 },
+                    { title: 'Card 3', cols: 1, rows: 1 },
+                    { title: 'Card 4', cols: 1, rows: 1 }
                 ];
             }
 
             return [
-                {title: 'Card 1', cols: 2, rows: 1},
-                {title: 'Card 2', cols: 1, rows: 1},
-                {title: 'Card 3', cols: 1, rows: 2},
-                {title: 'Card 4', cols: 1, rows: 1}
+                { title: 'Card 1', cols: 2, rows: 1 },
+                { title: 'Card 2', cols: 1, rows: 1 },
+                { title: 'Card 3', cols: 1, rows: 2 },
+                { title: 'Card 4', cols: 1, rows: 1 }
             ];
         })
     );
 
-    constructor(private breakpointObserver: BreakpointObserver) {
+    constructor(context: PageContext, private breakpointObserver: BreakpointObserver) {
+        super(context);
+        this.StateService.setTitle('Главная');
     }
 }
