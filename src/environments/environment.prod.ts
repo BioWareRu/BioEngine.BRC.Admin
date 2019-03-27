8c2dde154f99c85625eadaee5b4ec7d85d6c29d7
George <sonicgd@gmail.com>
2019-03-27 16:50:34 +0500

    fix prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { baseEnvironment } from './environment.base';
 
 baseEnvironment.hmr = false;
 baseEnvironment.production = true;
-baseEnvironment.apiUrl = 'https://api.brcgames.ru/v1';
+baseEnvironment.apiUrl = 'https://api.brcgames.ru/v1/';
 baseEnvironment.oauth.clientId = '8a7af1cc352af495fa0d95e7f3aed182';
 
 export const environment = baseEnvironment;
