a32abf9ea15f0fbdbb09ad2e424a205c235cd9d5
George <sonicgd@gmail.com>
2019-03-27 17:03:02 +0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 const routes: Routes = [
     { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-    { path: 'posts', loadChildren: './posts/posts.module#SitesModule' },
+    { path: 'posts', loadChildren: './posts/posts.module#PostsModule' },
     { path: 'sites', loadChildren: './sites/sites.module#SitesModule' },
     { path: 'sections', loadChildren: './sections/sections.module#SectionsModule' },
     { path: 'tags', loadChildren: './tags/tags.module#TagsModule' },
