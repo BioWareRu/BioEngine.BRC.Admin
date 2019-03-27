cd38e7a544e6e76953f172d3726c8a8d94819a61
George <sonicgd@gmail.com>
2019-03-27 16:37:55 +0500

    fix prod oauth client id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-import {baseEnvironment} from './environment.base';
+import { baseEnvironment } from './environment.base';
 
 baseEnvironment.hmr = false;
 baseEnvironment.production = true;
 baseEnvironment.apiUrl = 'https://api.brcgames.ru/v1';
-baseEnvironment.oauth.clientId = 'admin';
+baseEnvironment.oauth.clientId = '8a7af1cc352af495fa0d95e7f3aed182';
 
 export const environment = baseEnvironment;
 
