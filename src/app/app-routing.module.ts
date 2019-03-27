e154ef5b8b74d658445f19f2ef0cd3665b44c35a
George <sonicgd@gmail.com>
2019-03-27 16:51:26 +0500

    use special loadChildren syntax to work without in aot build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,15 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { MenuModule } from './menu/menu.module';
-import { PagesModule } from './pages/pages.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { SitesModule } from './sites/sites.module';
-import { SectionsModule } from './sections/sections.module';
-import { TagsModule } from './tags/tags.module';
-import { PostsModule } from './posts/posts.module';
-import { StorageModule } from './storage/storage.module';
 
 const routes: Routes = [
-    { path: 'dashboard', loadChildren: () => DashboardModule },
-    { path: 'posts', loadChildren: () => PostsModule },
-    { path: 'sites', loadChildren: () => SitesModule },
-    { path: 'sections', loadChildren: () => SectionsModule },
-    { path: 'tags', loadChildren: () => TagsModule },
-    { path: 'pages', loadChildren: () => PagesModule },
-    { path: 'menu', loadChildren: () => MenuModule },
-    { path: 'storage', loadChildren: () => StorageModule },
+    { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+    { path: 'posts', loadChildren: './posts/posts.module#SitesModule' },
+    { path: 'sites', loadChildren: './sites/sites.module#SitesModule' },
+    { path: 'sections', loadChildren: './sections/sections.module#SectionsModule' },
+    { path: 'tags', loadChildren: './tags/tags.module#TagsModule' },
+    { path: 'pages', loadChildren: './pages/pages.module#PagesModule' },
+    { path: 'menu', loadChildren: './menu/menu.module#MenuModule' },
+    { path: 'storage', loadChildren: './storage/storage.module#StorageModule' },
     { path: '**', redirectTo: 'dashboard' }
 ];
 
@@ -27,4 +19,4 @@ const config: ExtraOptions = {};
     imports: [RouterModule.forRoot(routes, config)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
