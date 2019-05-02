50c9737807fd83ecf38be041822698b62bd7bb83
George <sonicgd@gmail.com>
2019-05-02 11:13:31 +0500

    Don't patch blocks if nothing changes

--- src/app/@common/blocks/editor/blocks-form.component.ts
+++ src/app/@common/blocks/editor/blocks-form.component.ts
@@ -29,6 +29,10 @@ import { TwitchBlockFormComponent } from './twitchblock-form.component';
     encapsulation: ViewEncapsulation.None
 })
 export class BlocksFormComponent implements OnInit {
+
+    public constructor(private readonly _dialogsService: DialogService) {
+    }
+
     @Input()
     public form: Form;
 
@@ -36,7 +40,49 @@ export class BlocksFormComponent implements OnInit {
     public model: IContentEntity;
     blocksManager: BlocksManager;
 
-    public constructor(private readonly _dialogsService: DialogService) {
+    public static equals(x, y): boolean {
+        if (x === y) {
+            return true;
+        }
+        // if both x and y are null or undefined and exactly the same
+        if (!(x instanceof Object) || !(y instanceof Object)) {
+            return false;
+        }
+        // if they are not strictly equal, they both need to be Objects
+        if (x.constructor !== y.constructor) {
+            return false;
+        }
+        // they must have the exact same prototype chain, the closest we can do is
+        // test there constructor.
+
+        let p;
+        for (p in x) {
+            if (!x.hasOwnProperty(p)) {
+                continue;
+            }
+            // other properties were tested using x.constructor === y.constructor
+            if (!y.hasOwnProperty(p)) {
+                return false;
+            }
+            // allows to compare x[ p ] and y[ p ] when set to undefined
+            if (x[p] === y[p]) {
+                continue;
+            }
+            // if they have the same strict value or identity then they are equal
+            if (typeof (x[p]) !== 'object') {
+                return false;
+            }
+            // Numbers, Strings, Functions, Booleans must be strictly equal
+            if (!BlocksFormComponent.equals(x[p], y[p])) {
+                return false;
+            }
+        }
+        for (p in y) {
+            if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     ngOnInit(): void {
@@ -52,14 +98,17 @@ export class BlocksFormComponent implements OnInit {
         this.blocksManager.registerBlockType(ContentBlockItemType.Twitch, TwitchBlock, TwitchBlockFormComponent);
 
         this.blocksManager.blocks.subscribe(blocks => {
-            this.form.getControlByProperty('blocks').patchValue(blocks);
+            if (!BlocksFormComponent.equals(blocks, this.model.blocks)) {
+                this.form.getControlByProperty('blocks').patchValue(blocks);
+            } else {
+                console.log('equals');
+            }
         });
 
         if (this.model.blocks.length === 0) {
             this.blocksManager.addBlock(this.blocksManager.createBlock(ContentBlockItemType.Text));
             this.blocksManager.update();
         }
-
     }
 
     public addBlock(type: ContentBlockItemType): void {
@@ -72,9 +121,9 @@ export class BlocksFormComponent implements OnInit {
         this._dialogsService
             .confirm('Удаление блока', 'Вы точно хотите удалить это блок?')
             .onConfirm.subscribe(() => {
-                this.blocksManager.removeBlock(block);
-                this.blocksManager.update();
-            });
+            this.blocksManager.removeBlock(block);
+            this.blocksManager.update();
+        });
     }
 
     public drop(event: CdkDragDrop<Array<string>>): void {
